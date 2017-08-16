6b30842f8927887fbd566bed826e3072a8fa7aa0
AndreiLukashevich <lukashevichandn@gmail.com>
2017-08-16 15:07:10 +0300

    Update UI

--- src/app/modal-windows/ngx-moddialog/ngx-moddialog.component.ts
+++ src/app/modal-windows/ngx-moddialog/ngx-moddialog.component.ts
@@ -23,8 +23,70 @@ export class NgxModdialogComponent implements OnInit {
     this.router.navigate(["modalwindows"]);
   }
 
-  onClick() {
+  onClickAlert() {
     const dialogRef = this.modal.alert()
+      .size('sm')
+      .isBlocking(true)
+      .showClose(true)
+      .title('A simple Alert style modal window')
+      .body(`
+            <h4>Alert is a classic (title/body/footer) 1 button modal window that 
+            does not block.</h4>
+            <b>Configuration:</b>
+            <ul>
+                <li>Non blocking (click anywhere outside to dismiss)</li>
+                <li>Size large</li>
+                <li>Dismissed with default keyboard key (ESC)</li>
+                <li>Close wth button click</li>
+                <li>HTML content</li>
+            </ul>
+            <input />
+            `)
+      .okBtn('Ok')
+      .open();
+
+    dialogRef
+      .then(dialogRef => {
+        dialogRef.result.then(
+          result => { console.log(`${result}`) },
+          result => { console.log(`${result}`) }
+        );
+      });
+  }
+
+  onClickPrompt() {
+    const dialogRef = this.modal.prompt()
+      .size('lg')
+      .isBlocking(true)
+      .showClose(true)
+      .title('A simple Alert style modal window')
+      .body(`
+            <h4>Alert is a classic (title/body/footer) 1 button modal window that 
+            does not block.</h4>
+            <b>Configuration:</b>
+            <ul>
+                <li>Non blocking (click anywhere outside to dismiss)</li>
+                <li>Size large</li>
+                <li>Dismissed with default keyboard key (ESC)</li>
+                <li>Close wth button click</li>
+                <li>HTML content</li>
+            </ul>
+            <input />
+            `)
+      .okBtn('Submit')
+      .open();
+
+    dialogRef
+      .then(dialogRef => {
+        dialogRef.result.then(
+          result => { console.log(`${result}`) },
+          result => { console.log(`${result}`) }
+        );
+      });
+  }
+
+  onClickConfirm() {
+    const dialogRef = this.modal.confirm()
       .size('lg')
       .isBlocking(true)
       .showClose(true)
