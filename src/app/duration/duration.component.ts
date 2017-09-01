b219943cd1611ee4ac4ffc3d38685f9fb1fe7ddb
AndreiLukashevich <lukashevichandn@gmail.com>
2017-09-01 09:42:59 +0300

    Added the test version of duration component

--- src/app/duration/duration.component.ts
+++ src/app/duration/duration.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Clock {
+  constructor(
+    public time: any) {
+  }
+}
+
 @Component({
   moduleId: module.id,
   selector: 'app-duration',
@@ -8,9 +14,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DurationComponent implements OnInit {
 
+  clock: any = "";
+  visible: boolean = false;
+  hoursInMinute: number;
+  hours: any;
+  minute: number;
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  model: Clock = new Clock(null);
+  onKeyup(data) {
+    this.hours = data / 60;
+    this.minute = data % 60;
+    this.hoursInMinute = data - this.minute;
+    this.hours = this.hoursInMinute / 60;
+    console.log(isNaN(this.hours));
+    if (!isNaN(this.hours)) {
+      console.log(this.hours);
+      this.visible = false;
+      if (this.minute < 10) this.clock = this.hours + ":0" + this.minute;
+      else {
+        this.clock = this.hours + ":" + this.minute;
+      }
+    }
+    else {
+      this.visible = true;
+    }
   }
 
 }
